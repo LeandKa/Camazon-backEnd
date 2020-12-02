49421f0b5f12775cdc9bb042ab7f067cc5bb548e
LeandKa <lcavalcanti29@gmail.com>
2020-12-02 17:52:30 -0300

    fix imgs

--- data.js
+++ data.js
@@ -19,7 +19,7 @@
         {
             name:'Nike Slim Shirt',
             category:'Shirts',
-            image:'/images/p1.jpg',
+            image:'https://image.freepik.com/foto-gratis/jugueton-modelo-negro-sonriente-jeans-grises-camiseta-negra-algodon-liso-taburete-aislado-blanco_346278-1017.jpg',
             price:120,
             countInStock:10,
             brand:'Nike',
@@ -30,7 +30,7 @@
         {
             name:'Puma Large Pants',
             category:'Pants',
-            image:'/images/p3.jpg',
+            image:'https://image.freepik.com/foto-gratis/adolescente-masculino-pie-contra-pared-gris_23-2148205459.jpg',
             price:20,
             countInStock:20,
             brand:'Puma',
@@ -42,7 +42,7 @@
 
             name:'Nike Slim Shirt',
             category:'Shirts',
-            image:'/images/p6.jpg',
+            image:'https://image.freepik.com/foto-gratis/hombre-gafas-sol-camiseta-blanca-posando_158538-16721.jpg',
             price:1200,
             brand:'Nike',
             countInStock:0,
@@ -53,7 +53,7 @@
         {
             name:'Puma Slim Pants',
             category:'Pants',
-            image:'/images/p3.jpg',
+            image:'https://image.freepik.com/foto-gratis/jugueton-modelo-negro-sonriente-jeans-grises-camiseta-negra-algodon-liso-taburete-aislado-blanco_346278-1017.jpg',
             price:40,
             countInStock:12,
             brand:'Puma',
