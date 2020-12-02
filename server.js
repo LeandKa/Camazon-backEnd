5deff751a915bdb53c29a46568c97ddf1d5c857f
LeandKa <lcavalcanti29@gmail.com>
2020-12-02 15:12:29 -0300

    fixes

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
     res.status(500).send({ messag: err.message });
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Estou sendo lido')
 })
