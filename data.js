4dafacc3424ea5fd31a83d3bbeb4e7035c1d26cf
LeandKa <lcavalcanti29@gmail.com>
2020-12-02 16:11:56 -0300

    fixs

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
  const data = {
      users:[
        {
-           name:'Basir',
+           name:'Leandro',
            email:'admin@example.com',
            password: bcrypt.hashSync('1234',8),
            isAdmin:true,
